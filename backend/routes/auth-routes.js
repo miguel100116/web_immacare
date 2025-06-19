ecc8639d1953541c72210f506dcf7b4e0569c459
Jatrish De Guzman <deguzmanjatrish@gmail.com>
2025-06-19 17:38:53 +0800

    modified:   backend/routes/auth-routes.js

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -56,6 +56,7 @@ router.post('/check-email', async (req, res) => {
 router.post('/post', async (req, res) => {
     const { firstName, lastName, suffix, signupEmail, Age, Sex, PhoneNumber, Address, signupPassword, confirmPassword } = req.body;
     const BASE_URL = `https://web-immacare.onrender.com` || `http://localhost:${port}`;
+          const validPhilippinesPhone = /^(09|\+639)\d{9}$/;
     if (signupPassword !== confirmPassword) {
         return res.status(400).json({ error: "Passwords do not match" });
     }
@@ -72,7 +73,7 @@ router.post('/post', async (req, res) => {
         if (!validPhilippinesPhone.test(PhoneNumber)) {
          return res.status(400).json({ error: "Please enter a valid Philippine mobile number (e.g., 09171234567 or +639171234567)" });
         }
-        const validPhilippinesPhone = /^(09|\+639)\d{9}$/;
+  
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(signupPassword, saltRounds);
 
